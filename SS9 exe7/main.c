8ef35efba6c08438a4a10073e366ebf59d69625c
mxt629 <xuantien.06021990@gmail.com>
2022-03-21 22:21:42 +0700

    Initial Commit

---
+++ SS9 exe7/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  SS9 exe7
+//
+//  Created by Little Chick on 21/03/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
